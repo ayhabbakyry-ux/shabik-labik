ee618ec6f1e2af1337e40b27efbcca947fd4d568
Ayhab Bakyry <ayhabbakyry@gmail.com>
2026-07-10 00:45:40 +0000

    Try fixing this error: `Build Error: Ecmascript file had an error. Error

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,5 @@
-export const dynamic = 'force-dynamic';
 "use client";
+export const dynamic = 'force-dynamic';
 
 import { AdminPanel } from "@/components/admin/AdminPanel";
 import { Navbar, DesktopHeader } from "@/components/layout/Navbar";
